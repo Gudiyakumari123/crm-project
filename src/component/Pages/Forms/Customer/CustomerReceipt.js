9d376e7d05363b928777898c1cf7c5eb9379064b
Gudiyakumari123 <gudiyakuamri.20mca@jyotinivas.org>
2022-11-09 10:38:57 +0530

    dd

--- src/component/Pages/Forms/Customer/CustomerReceipt.js
+++ src/component/Pages/Forms/Customer/CustomerReceipt.js
@@ -271,248 +271,248 @@
 // export default CustomerReceipt;
 
 
-import React, { useState, useEffect, Fragment } from "react";
+// import React, { useState, useEffect, Fragment } from "react";
 
-// components
-import Header from "../../../../shared/Header/Header";
-import SubHeader from "../../../../shared/SubHeader/SubHeader";
-import Footer from "../../../../shared/Footer/Footer";
-import { Button } from "react-bootstrap";
-import { ToastContainer, toast, cssTransition } from "react-toastify";
+// // components
+// import Header from "../../../../shared/Header/Header";
+// import SubHeader from "../../../../shared/SubHeader/SubHeader";
+// import Footer from "../../../../shared/Footer/Footer";
+// import { Button } from "react-bootstrap";
+// import { ToastContainer, toast, cssTransition } from "react-toastify";
 
-// Reusable Component
-import Input from "../../../../shared/Reusable/Input";
-import Select from "../../../../shared/Reusable/Select";
-import TextArea from "../../../../shared/Reusable/TextArea";
+// // Reusable Component
+// import Input from "../../../../shared/Reusable/Input";
+// import Select from "../../../../shared/Reusable/Select";
+// import TextArea from "../../../../shared/Reusable/TextArea";
 
-// Phone Number
-import PhoneInput from "react-phone-number-input";
-import "react-phone-number-input/style.css";
-import { render } from "@testing-library/react";
+// // Phone Number
+// import PhoneInput from "react-phone-number-input";
+// import "react-phone-number-input/style.css";
+// import { render } from "@testing-library/react";
 
-const bounce = cssTransition({
-  enter: "animate__animated animate__bounceIn",
-  exit: "animate__animated animate__bounceOut",
-});
+// const bounce = cssTransition({
+//   enter: "animate__animated animate__bounceIn",
+//   exit: "animate__animated animate__bounceOut",
+// });
 
-const swirl = cssTransition({
-  enter: "swirl-in-fwd",
-  exit: "swirl-out-bck",
-});
-
-
-const BillNo = [
-  { value: "Bill No 1", label: "Bill No 1" },
-  { value: "Bill No 2", label: "Bill No 2" },
-  { value: "Bill No 3", label: "Bill No 3" },
-];
-
-const payMode = [
-  { value: "Cash", label: "Cash" },
-  { value: "Card", label: "Card" },
-  { value: "UPI", label: "UPI" },
-];
+// const swirl = cssTransition({
+//   enter: "swirl-in-fwd",
+//   exit: "swirl-out-bck",
+// });
 
 
+// const BillNo = [
+//   { value: "Bill No 1", label: "Bill No 1" },
+//   { value: "Bill No 2", label: "Bill No 2" },
+//   { value: "Bill No 3", label: "Bill No 3" },
+// ];
 
-class CustomerReceipt extends Component {
-
-  initialState = {
-    version: "",
-    customerId: "",
-    companyName: "",
-    mobileNumber: "",
-    city: "",
-    amount: "",
-    discountPer: "",
-    discountAmt: "",
-    grossAmt: "",
-    gst: "",
-    totalTaxAmount: "",
-    roundAmt: "",
-    netAmt: "",
-    paidAmt: "",
-    details: "",
-    date: "",
-    remarks: "",
-    dates: ""
-  };
-  state = this.initialState;
-
-  handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues({
-      ...formValues,
-      [name]: value,
-    });
-    setPhone();
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    setFormErrors(validate(formValues));
-    setIsSubmit(true);
-    setFormValues(initialValues);
-    // animateCss();  
-  };
-
-  validate = () => {
-    let name = this.state.name;
-    let errors = {};
-    let isValid = true;
-
-    if (!name) {
-      isValid = false;
-      errors["name"] = "Enter Valid Value";
-    }
-    return isValid;
-  };
-  handleReset = () => {
-    this.setState(() => this.initialState);
-  };
+// const payMode = [
+//   { value: "Cash", label: "Cash" },
+//   { value: "Card", label: "Card" },
+//   { value: "UPI", label: "UPI" },
+// ];
 
 
 
+// class CustomerReceipt extends Component {
 
-  render() {
-    return (
-      <>
-        <Header />
-        <SubHeader />
-        <div className="form__container">
-          <div className="form__content">
-            <div className="title-display">
-              <div className="title"> Customer Receipt </div>
-            </div>
-            <div className="form__wrapper">
-              <div className="form__left">
-                <div className="fields">
-                  <Input
-                    type="text"
-                    label="CustomerId"
-                    style={{
-                      marginLeft: "-70px",
-                      width: "100%",
-                    }}
-                    name="customerId"
-                    value={formValues.customerId}
-                    onChange={this.handleChange}
-                    isError
-                    errorMsg={formErrors.customerId}
-                  />
-                </div>
-                <p className="show-errors-left"> {formErrors.customerId}</p>
+//   initialState = {
+//     version: "",
+//     customerId: "",
+//     companyName: "",
+//     mobileNumber: "",
+//     city: "",
+//     amount: "",
+//     discountPer: "",
+//     discountAmt: "",
+//     grossAmt: "",
+//     gst: "",
+//     totalTaxAmount: "",
+//     roundAmt: "",
+//     netAmt: "",
+//     paidAmt: "",
+//     details: "",
+//     date: "",
+//     remarks: "",
+//     dates: ""
+//   };
+//   state = this.initialState;
+
+//   handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setFormValues({
+//       ...formValues,
+//       [name]: value,
+//     });
+//     setPhone();
+//   };
+
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+//     setFormErrors(validate(formValues));
+//     setIsSubmit(true);
+//     setFormValues(initialValues);
+//     // animateCss();  
+//   };
+
+//   validate = () => {
+//     let name = this.state.name;
+//     let errors = {};
+//     let isValid = true;
+
+//     if (!name) {
+//       isValid = false;
+//       errors["name"] = "Enter Valid Value";
+//     }
+//     return isValid;
+//   };
+//   handleReset = () => {
+//     this.setState(() => this.initialState);
+//   };
 
 
-                <div className="fields">
-                  <Input
-                    type="text"
-                    label="Company Name"
-                    style={{
-                      marginLeft: "-70px",
-                      width: "100%",
-                    }}
-                    name="companyName"
-                    value={formValues.companyName}
-                    onChange={this.handleChange}
-                    isError
-                    errorMsg={formErrors.companyName}
-                  />
-                </div>
-                <p className="show-errors-left"> {formErrors.companyName}</p>
 
 
-                <div className="fields">
-                  <div className="input-fields">
-                    <label htmlFor="" className="label">
-                      Phone
+//   render() {
+//     return (
+//       <>
+//         <Header />
+//         <SubHeader />
+//         <div className="form__container">
+//           <div className="form__content">
+//             <div className="title-display">
+//               <div className="title"> Customer Receipt </div>
+//             </div>
+//             <div className="form__wrapper">
+//               <div className="form__left">
+//                 <div className="fields">
+//                   <Input
+//                     type="text"
+//                     label="CustomerId"
+//                     style={{
+//                       marginLeft: "-70px",
+//                       width: "100%",
+//                     }}
+//                     name="customerId"
+//                     value={formValues.customerId}
+//                     onChange={this.handleChange}
+//                     isError
+//                     errorMsg={formErrors.customerId}
+//                   />
+//                 </div>
+//                 <p className="show-errors-left"> {formErrors.customerId}</p>
 
-                    </label>
-                    <PhoneInput
-                      style={{
-                        marginLeft: "-70px",
-                      }}
-                      value={phone}
-                      name="mobileNumber"
-                      className="form-control"
-                      international
-                      defaultCountry="IN"
-                      onChange={setPhone}
-                    />
-                  </div>
-                </div>
-                <div className="fields">
-                  <Select
-                    label="Install"
-                    placeholder="Select Installation No."
-                    options={BillNo}
-                    defaultValue={BillNo[0]}
-                    className="select-control bill-select department-select"
-                  />
-                </div>
 
-              </div>
-              {/* Left Side End */}
-              {/* Right Side Start */}
-              <div className="form__right">
-                <div className="fields">
-                  <Select
-                    label="PayMode"
-                    options={payMode}
-                    className="select-control source-select"
-                    defaultValue={payMode[0]}
-                    isError
-                  />
-                </div>
+//                 <div className="fields">
+//                   <Input
+//                     type="text"
+//                     label="Company Name"
+//                     style={{
+//                       marginLeft: "-70px",
+//                       width: "100%",
+//                     }}
+//                     name="companyName"
+//                     value={formValues.companyName}
+//                     onChange={this.handleChange}
+//                     isError
+//                     errorMsg={formErrors.companyName}
+//                   />
+//                 </div>
+//                 <p className="show-errors-left"> {formErrors.companyName}</p>
 
-                <div className="fields">
-                  <Input
-                    type="text"
-                    label="PaidAmt"
-                    name="paidAmt"
-                    value={formValues.paidAmt}
-                    onChange={this.handleChange}
-                    isError
-                    errorMsg={formErrors.paidAmt}
-                  />
-                </div>
-                <p className="show-errors"> {formErrors.paidAmt}</p>
 
-                <div className="fields">
-                  <TextArea
-                    type="text"
-                    label="Remarks"
-                    rows="2"
-                    name="remarks"
-                    value={formValues.remarks}
-                    onChange={this.handleChange}
-                    style={{
-                      marginLeft: "13px"
-                    }}
-                  />
-                </div>
+//                 <div className="fields">
+//                   <div className="input-fields">
+//                     <label htmlFor="" className="label">
+//                       Phone
 
-              </div>
-            </div>
-            <div className="btn__holder">
-              <Button
-                type="submit"
-                className="btn btn-primary"
-                // onClick={animateCss}
-                onClick={this.handleSubmit}
-              >Save
-              </Button>
-              <Button className="btn btn-secondary"
-                onClick={this.clearForm}
-              >Clear</Button>
-            </div>
-          </div>
-        </div>
-        <ToastContainer />
-        <Footer />
-        {/* Source Modal*/}
-      </>
-    );
-  };
-};
-export default CustomerReceipt;
+//                     </label>
+//                     <PhoneInput
+//                       style={{
+//                         marginLeft: "-70px",
+//                       }}
+//                       value={phone}
+//                       name="mobileNumber"
+//                       className="form-control"
+//                       international
+//                       defaultCountry="IN"
+//                       onChange={setPhone}
+//                     />
+//                   </div>
+//                 </div>
+//                 <div className="fields">
+//                   <Select
+//                     label="Install"
+//                     placeholder="Select Installation No."
+//                     options={BillNo}
+//                     defaultValue={BillNo[0]}
+//                     className="select-control bill-select department-select"
+//                   />
+//                 </div>
+
+//               </div>
+//               {/* Left Side End */}
+//               {/* Right Side Start */}
+//               <div className="form__right">
+//                 <div className="fields">
+//                   <Select
+//                     label="PayMode"
+//                     options={payMode}
+//                     className="select-control source-select"
+//                     defaultValue={payMode[0]}
+//                     isError
+//                   />
+//                 </div>
+
+//                 <div className="fields">
+//                   <Input
+//                     type="text"
+//                     label="PaidAmt"
+//                     name="paidAmt"
+//                     value={formValues.paidAmt}
+//                     onChange={this.handleChange}
+//                     isError
+//                     errorMsg={formErrors.paidAmt}
+//                   />
+//                 </div>
+//                 <p className="show-errors"> {formErrors.paidAmt}</p>
+
+//                 <div className="fields">
+//                   <TextArea
+//                     type="text"
+//                     label="Remarks"
+//                     rows="2"
+//                     name="remarks"
+//                     value={formValues.remarks}
+//                     onChange={this.handleChange}
+//                     style={{
+//                       marginLeft: "13px"
+//                     }}
+//                   />
+//                 </div>
+
+//               </div>
+//             </div>
+//             <div className="btn__holder">
+//               <Button
+//                 type="submit"
+//                 className="btn btn-primary"
+//                 // onClick={animateCss}
+//                 onClick={this.handleSubmit}
+//               >Save
+//               </Button>
+//               <Button className="btn btn-secondary"
+//                 onClick={this.clearForm}
+//               >Clear</Button>
+//             </div>
+//           </div>
+//         </div>
+//         <ToastContainer />
+//         <Footer />
+//         {/* Source Modal*/}
+//       </>
+//     );
+//   };
+// };
+// export default CustomerReceipt;
